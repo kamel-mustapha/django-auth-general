bac848d787a06e6e4e2e224ce529e5cbddd3f187
musk96 <musk96@hotmail.fr>
2024-11-21 22:11:35 +0100

    project init

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+API_URL = "/api/v1/"
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path(f'{API_URL}auth/', include('user.urls'))
+]
